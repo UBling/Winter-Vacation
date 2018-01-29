dcd7ae375fcb525da65c42971aa1d780c2890c2d
张玥瑶 <35864675+UBling@users.noreply.github.com>
2018-01-29 18:13:16 +0800

    Add files via upload

---
+++ basic2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.14
+#define N (4.0f / 3.0f)
+
+int main()
+{
+    float r;
+
+    printf("Please input the r\n:");
+    scanf("%f",&r);
+
+    printf("V = %.1f", PI*N*r*r*r);
+
+    return 0;
+}
